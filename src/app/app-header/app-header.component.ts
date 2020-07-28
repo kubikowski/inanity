a2ea0ebf6c9e1c553930a83f40cf318c1f5f5c0c
kubikowski <kubikowski0@gmail.com>
2020-07-28 16:48:46 -0400

    Moved color switching to ColorsService

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ColorPalette } from '../shared/colors/models/color-palette.model';
 import { ColorTheme } from '../shared/colors/models/color-theme.model';
+import { ColorsService } from '../shared/colors/services/colors.service';
 
 @Component({
 	selector: 'app-header',
@@ -9,82 +10,25 @@ import { ColorTheme } from '../shared/colors/models/color-theme.model';
 })
 export class AppHeaderComponent implements OnInit {
 
-	body = document.body;
-	style = document.documentElement.style;
-
 	// Color Themes
-	rootTheme = ColorTheme.getRootTheme();
 	colorThemes = ColorTheme.getThemes();
 
 	// Color Palettes
-	rootPalette = ColorPalette.getRootPalette();
 	colorPalettes = ColorPalette.getPalettes();
 
-	constructor() {
+	constructor(private colorsService: ColorsService) {
 	}
 
 	ngOnInit(): void {
-		this.body.classList.add(this.colorThemes[0].themeName);
-		this.body.classList.add(this.colorPalettes[0].paletteName);
 	}
 
 	toggleTheme(theme: ColorTheme, event: Event): void {
 		event.stopPropagation();
-
-		this.body.classList.remove('light-theme', 'dark-theme');
-		this.body.classList.add(theme.themeName);
-
-		this.setTheme(theme);
-		this.setPalette(this.getPalette());
+		this.colorsService.toggleTheme(theme);
 	}
 
 	togglePalette(palette: ColorPalette, event: Event): void {
 		event.stopPropagation();
-
-		this.body.classList.remove(...this.colorPalettes.map(colorPalette => colorPalette.paletteName));
-		this.body.classList.add(palette.paletteName);
-
-		this.setPalette(palette);
-	}
-
-	getTheme(): ColorTheme {
-		for (const colorTheme of this.colorThemes) {
-			if (this.body.classList.contains(colorTheme.themeName)) {
-				return colorTheme;
-			}
-		}
-		return null;
-	}
-
-	setTheme(theme: ColorTheme): void {
-		Object.entries(this.rootTheme).forEach(([key, value]) => {
-			this.style.setProperty( value, `var(${theme[key]})`);
-		});
-	}
-
-	getPalette(): ColorPalette {
-		for (const colorPalette of this.colorPalettes) {
-			if (this.body.classList.contains(colorPalette.paletteName)) {
-				return colorPalette;
-			}
-		}
-		return null;
-	}
-
-	getComputedPalette(palette: ColorPalette): ColorPalette {
-		switch (this.getTheme().themeName) {
-			case 'dark-theme':
-				return ColorPalette.getInverse(palette);
-			case 'light-theme':
-			default:
-				return palette;
-		}
-	}
-
-	setPalette(palette: ColorPalette): void {
-		const computedPalette = this.getComputedPalette(palette);
-		Object.entries(this.rootPalette).forEach(([key, value]) => {
-			this.style.setProperty( value, `var(${computedPalette[key]})`);
-		});
+		this.colorsService.togglePalette(palette);
 	}
 }
--- src/app/shared/colors/services/colors.service.ts
+++ src/app/shared/colors/services/colors.service.ts
@@ -1,10 +1,82 @@
 import { Injectable } from '@angular/core';
+import { ColorTheme } from '../models/color-theme.model';
+import { ColorPalette } from '../models/color-palette.model';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class ColorsService {
 
+	// Document
+	body = document.body;
+	style = document.documentElement.style;
+
+	// Color Themes
+	rootTheme = ColorTheme.getRootTheme();
+	colorThemes = ColorTheme.getThemes();
+
+	// Color Palettes
+	rootPalette = ColorPalette.getRootPalette();
+	colorPalettes = ColorPalette.getPalettes();
+
 	constructor() {
+		this.body.classList.add(this.colorThemes[0].themeName);
+		this.body.classList.add(this.colorPalettes[0].paletteName);
+	}
+
+	toggleTheme(theme: ColorTheme): void {
+		this.body.classList.remove('light-theme', 'dark-theme');
+		this.body.classList.add(theme.themeName);
+
+		this.setTheme(theme);
+		this.setPalette(this.getPalette());
+	}
+
+	togglePalette(palette: ColorPalette): void {
+		this.body.classList.remove(...this.colorPalettes.map(colorPalette => colorPalette.paletteName));
+		this.body.classList.add(palette.paletteName);
+
+		this.setPalette(palette);
+	}
+
+	getTheme(): ColorTheme {
+		for (const colorTheme of this.colorThemes) {
+			if (this.body.classList.contains(colorTheme.themeName)) {
+				return colorTheme;
+			}
+		}
+		return null;
+	}
+
+	setTheme(theme: ColorTheme): void {
+		Object.entries(this.rootTheme).forEach(([key, value]) => {
+			this.style.setProperty( value, `var(${theme[key]})`);
+		});
+	}
+
+	getPalette(): ColorPalette {
+		for (const colorPalette of this.colorPalettes) {
+			if (this.body.classList.contains(colorPalette.paletteName)) {
+				return colorPalette;
+			}
+		}
+		return null;
+	}
+
+	getComputedPalette(palette: ColorPalette): ColorPalette {
+		switch (this.getTheme().themeName) {
+			case 'dark-theme':
+				return ColorPalette.getInverse(palette);
+			case 'light-theme':
+			default:
+				return palette;
+		}
+	}
+
+	setPalette(palette: ColorPalette): void {
+		const computedPalette = this.getComputedPalette(palette);
+		Object.entries(this.rootPalette).forEach(([key, value]) => {
+			this.style.setProperty( value, `var(${computedPalette[key]})`);
+		});
 	}
 }
