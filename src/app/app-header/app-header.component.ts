7c86dbc0e70a7332de3cd862c46d93f2c395ebf5
kubikowski <kubikowski0@gmail.com>
2020-07-28 13:20:52 -0400

    Added ColorTheme model Added repeated color themes from menu

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ColorTheme } from '../shared/color-themes/models/color-theme';
 
 @Component({
 	selector: 'app-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./app-header.component.scss']
 })
 export class AppHeaderComponent implements OnInit {
+
+	colorThemes = ColorTheme.getThemes();
 
 	constructor() {
 	}
