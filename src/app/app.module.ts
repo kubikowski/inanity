156de36ee5868c52744ed50d58bc4b88f29cace4
kubikowski <kubikowski0@gmail.com>
2020-07-28 21:32:07 -0400

    Added some junk on a card to fill the page's main content.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Material Components
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppThemePickerComponent } from './shared/app-theme-picker/app-theme-picker.component';
 
 @NgModule({
@@ -23,6 +27,7 @@ import { AppThemePickerComponent } from './shared/app-theme-picker/app-theme-pic
 		AppRoutingModule,
 		BrowserAnimationsModule,
 		MatButtonModule,
+		MatCardModule,
 		MatDividerModule,
 		MatIconModule,
 		MatMenuModule,
