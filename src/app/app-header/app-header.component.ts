1a7f6cfcd25bc7db341633e8a35a108a236e1f1f
kubikowski <kubikowski0@gmail.com>
2020-07-28 20:39:08 -0400

    Added subsink, Dyslexic Title

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { timer } from 'rxjs/internal/observable/timer';
+import { SubSink } from 'subsink';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './app-header.component.html',
 	styleUrls: ['./app-header.component.scss']
 })
-export class AppHeaderComponent implements OnInit {
+export class AppHeaderComponent implements OnInit, OnDestroy {
+
+	subscriptions = new SubSink();
+
+	title = 'inanity';
+	dyslexicTitles: string[] = [
+		'iannity',
+		'innaity',
+		'inainty',
+		'inantiy',
+		'ininaty',
+		'inatiny',
+		'ianinty',
+		'innitay',
+		'ianitny',
+	];
 
 	constructor() {
 	}
 
 	ngOnInit(): void {
+		this.subscriptions.sink = timer(0, 2000)
+			.subscribe(() => this.title = this.getNewTitle());
+	}
+
+	ngOnDestroy(): void {
+		this.subscriptions.unsubscribe();
+	}
+
+	getNewTitle(): string {
+		let odds = Math.floor(Math.random() * 60);
+		for (const title of this.dyslexicTitles) {
+			if (odds === 0) {
+				return title;
+			}
+			odds--;
+		}
+		return 'inanity';
 	}
 }
