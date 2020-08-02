4fec47925cc58a9c502d35ba0df199bb5b38d236
kubikowski <kubikowski0@gmail.com>
2020-08-02 19:34:28 -0400

    Added angular routing and redirect to gong

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GongComponent } from './pages/gong/gong.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+		path: '',
+		redirectTo: '/gong',
+		pathMatch: 'full'
+	}, {
+		path: 'gong',
+		component: GongComponent,
+	// }, {
+	// 	path: '**',
+	// 	component: PageNotFoundComponent
+	},
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
