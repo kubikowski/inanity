99f08efc5362683a06ca16acd11bee3d0c7f904a
kubikowski <kubikowski0@gmail.com>
2020-08-06 18:57:11 -0400

    Added another errored route

--- src/app/navigation/sidebar/sidebar.component.ts
+++ src/app/navigation/sidebar/sidebar.component.ts
@@ -19,6 +19,7 @@ export class SidebarComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.navigationConfigs = [
+			NavigationConfig.from('About', 'wheel', 'about'),
 			NavigationConfig.from('Gong', 'gong', 'gong'),
 			NavigationConfig.from('404', 'wheel', 'get-got'),
 		];
