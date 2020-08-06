d4d233bc695931309b84d2d3f8d9bf53a66093e3
kubikowski <kubikowski0@gmail.com>
2020-08-06 17:59:54 -0400

    Moved mat card page styling to its own page.component.scss

--- src/app/pages/gong/gong.component.ts
+++ src/app/pages/gong/gong.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
 	selector: 'gong',
 	templateUrl: './gong.component.html',
-	styleUrls: ['./gong.component.scss']
+	styleUrls: ['./gong.component.scss', './../page.component.scss']
 })
 export class GongComponent implements OnInit {
 
