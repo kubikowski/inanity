b47f22f52c4e88f1d859dc4cb894ffc6f3549cd1
kubikowski <kubikowski0@gmail.com>
2020-08-22 01:32:17 -0400

    Improced Color unit tests by varying each red green and blue value from each other

--- src/app/shared/colors/models/color.model.spec.ts
+++ src/app/shared/colors/models/color.model.spec.ts
@@ -3,14 +3,14 @@ import { Color, InvalidColorString } from './color.model';
 describe('Color', () => {
 
 	/** region Test fromString & toString with RGB types */
-	it('should return "rgb(128, 128, 128)" -> "rgb(128, 128, 128)"', () => {
-		const toString = Color.fromString('rgb(128, 128, 128)').toString();
-		expect(toString).toBe('rgb(128, 128, 128)');
+	it('should return "rgb(127, 128, 129)" -> "rgb(127, 128, 129)"', () => {
+		const toString = Color.fromString('rgb(127, 128, 129)').toString();
+		expect(toString).toBe('rgb(127, 128, 129)');
 	});
 
-	it('should return "rgba(255,255, 255,  1)" -> "rgb(255, 255, 255)"', () => {
-		const toString = Color.fromString('rgba(255,255, 255,  1)').toString();
-		expect(toString).toBe('rgb(255, 255, 255)');
+	it('should return "rgba(253,254, 255 ,  1  )" -> "rgb(253, 254, 255)"', () => {
+		const toString = Color.fromString('rgba(253,254, 255 ,  1  )').toString();
+		expect(toString).toBe('rgb(253, 254, 255)');
 	});
 
 	it('should return "rgba(1, 2, 3, 0.4)" -> "rgba(1, 2, 3, 0.4)"', () => {
@@ -18,31 +18,31 @@ describe('Color', () => {
 		expect(toString).toBe('rgba(1, 2, 3, 0.4)');
 	});
 
-	it('should return "rgba(0, 0, 0, 0.87)" -> "rgba(0, 0, 0, 0.87)"', () => {
-		const toString = Color.fromString('rgba(0, 0, 0, 0.87)').toString();
-		expect(toString).toBe('rgba(0, 0, 0, 0.87)');
+	it('should return "rgba(0, 1, 2, 0.87)" -> "rgba(0, 1, 2, 0.87)"', () => {
+		const toString = Color.fromString('rgba(0, 1, 2, 0.87)').toString();
+		expect(toString).toBe('rgba(0, 1, 2, 0.87)');
 	});
 
-	it('should return "rgb(0, 0.1, 256)" -> "rgb(0, 0, 255)"', () => {
-		const toString = Color.fromString('rgb(0, 0.1, 256)').toString();
-		expect(toString).toBe('rgb(0, 0, 255)');
+	it('should return "rgb(0, 1.1, 256)" -> "rgb(0, 1, 255)"', () => {
+		const toString = Color.fromString('rgb(0, 1.1, 256)').toString();
+		expect(toString).toBe('rgb(0, 1, 255)');
 	});
 	/** endregion */
 
 	/** region Test fromString & toString with Hex types */
-	it('should return "#FFF" -> "rgb(255, 255, 255)"', () => {
-		const toString = Color.fromString('#FFF').toString();
-		expect(toString).toBe('rgb(255, 255, 255)');
+	it('should return "#DEF" -> "rgb(221, 238, 255)"', () => {
+		const toString = Color.fromString('#DEF').toString();
+		expect(toString).toBe('rgb(221, 238, 255)');
 	});
 
-	it('should return "#F000" -> "rgb(0, 0, 0)"', () => {
-		const toString = Color.fromString('#F000').toString();
-		expect(toString).toBe('rgb(0, 0, 0)');
+	it('should return "#F012" -> "rgb(0, 17, 34)"', () => {
+		const toString = Color.fromString('#F012').toString();
+		expect(toString).toBe('rgb(0, 17, 34)');
 	});
 
-	it('should return "#C000" -> "rgba(0, 0, 0, 0.8)"', () => {
-		const toString = Color.fromString('#C000').toString();
-		expect(toString).toBe('rgba(0, 0, 0, 0.8)');
+	it('should return "#CDEF" -> "rgba(221, 238, 255, 0.8)"', () => {
+		const toString = Color.fromString('#CDEF').toString();
+		expect(toString).toBe('rgba(221, 238, 255, 0.8)');
 	});
 
 	it('should return "#012345" -> "rgb(1, 35, 69)"', () => {
@@ -50,9 +50,9 @@ describe('Color', () => {
 		expect(toString).toBe('rgb(1, 35, 69)');
 	});
 
-	it('should return "#CC00DD00" -> "rgba(0, 221, 0, 0.8)"', () => {
-		const toString = Color.fromString('#CC00DD00').toString();
-		expect(toString).toBe('rgba(0, 221, 0, 0.8)');
+	it('should return "#ccddeeff" -> "rgba(221, 238, 255, 0.8)"', () => {
+		const toString = Color.fromString('#ccddeeff').toString();
+		expect(toString).toBe('rgba(221, 238, 255, 0.8)');
 	});
 	/** endregion */
 
