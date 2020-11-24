41a6d3eb694e7d18a233209ce1b7fcffa6d5ec2e
kubikowski <kubikowski0@gmail.com>
2020-11-23 21:19:20 -0500

    Still untested: removed some code from xor

--- src/app/shared/functions/xor/xor.function.ts
+++ src/app/shared/functions/xor/xor.function.ts
@@ -1,25 +1,10 @@
 /**
- * @param sets      an arbitrary number of sets.
- * @return          an array of values that exist in only in one of the input arrays.
- */
-export function xorSets<T>(...sets: Set<T>[]): Set<T> {
-	switch (sets.length) {
-		case 0:
-			return new Set();
-		case 1:
-			return sets[0];
-		default:
-			return xor(...sets);
-	}
-}
-
-/**
- * Creates two sets: singles & multiples.
+ * Returns the symmetric difference between some number of sets
+ *
+ * @impl Creates two sets: singles & multiples.
  * Loops over each value in each set and compares with multiples, then singles.
- *
- * @param sets The full array of sets for symmetric difference
- * @return the resulting singles array
- *
+ * @param sets  The full array of sets for symmetric difference.
+ * @return      The resulting singles array.
  * @runtime O(n*m)
  */
 function xor<T>(...sets: Set<T>[]): Set<T> {
