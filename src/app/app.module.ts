a4076b6529d3dc264e1b2483c6b02d9fba58e7dd
kubikowski <kubikowski0@gmail.com>
2021-03-16 21:44:53 -0400

    Added Node Dockerfile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { AppComponent } from './app.component';
 import { NavigationModule } from './navigation/navigation.module';
 import { SharedModule } from './shared/shared.module';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { SnekComponent } from './pages/page-not-found/snek/snek.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		PageNotFoundComponent,
+		SnekComponent,
 	],
 	imports: [
 		AppRoutingModule,
---
+++ src/app/pages/page-not-found/snek/snek.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'snek',
+	templateUrl: './snek.component.html',
+	styleUrls: ['./snek.component.scss']
+})
+export class SnekComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
+
+}
