97756383e553c8e9d47b8787c032556154d977ab
kubikowski <kubikowski0@gmail.com>
2021-03-21 11:00:23 -0400

    Added document:keyboardEvent listener

--- src/app/pages/page-not-found/snek/snek.component.ts
+++ src/app/pages/page-not-found/snek/snek.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Observed } from 'src/app/shared/decorators/observed.decorator';
-import { SnekGame } from 'src/app/pages/page-not-found/snek/models/snek-game.model';
-import { SnekGridNodeType } from 'src/app/pages/page-not-found/snek/models/snek-grid-node-type.enum';
-import { Observable } from 'rxjs';
-import { timer } from 'rxjs/internal/observable/timer';
-import { SubSink } from 'subsink';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {Observed} from 'src/app/shared/decorators/observed.decorator';
+import {SnekGame} from 'src/app/pages/page-not-found/snek/models/snek-game.model';
+import {SnekGridNodeType} from 'src/app/pages/page-not-found/snek/models/snek-grid-node-type.enum';
+import {Observable} from 'rxjs';
+import {timer} from 'rxjs/internal/observable/timer';
+import {SubSink} from 'subsink';
+import {SnekDirection} from './models/snek-direction.enum';
 
 @Component({
 	selector: 'snek',
@@ -20,8 +21,30 @@ export class SnekComponent implements OnInit {
 	public readonly SnekGridNodeType = SnekGridNodeType;
 
 	constructor() {
-		this.subscriptions.sink = timer(200, 200)
+		this.subscriptions.sink = timer(150, 150)
 			.subscribe(() => this.snekGame.snek.move());
+	}
+
+	@HostListener('document:keydown', [ '$event' ])
+	private keyDown(keyboardEvent: KeyboardEvent): void {
+		switch (keyboardEvent.key) {
+			case 'w':
+			case 'ArrowUp':
+				this.snekGame.snek.direction = SnekDirection.UP;
+				break;
+			case 's':
+			case 'ArrowDown':
+				this.snekGame.snek.direction = SnekDirection.DOWN;
+				break;
+			case 'a':
+			case 'ArrowLeft':
+				this.snekGame.snek.direction = SnekDirection.LEFT;
+				break;
+			case 'd':
+			case 'ArrowRight':
+				this.snekGame.snek.direction = SnekDirection.RIGHT;
+				break;
+		}
 	}
 
 	ngOnInit(): void {
