8e856f70e1922c801cf7c0d468aa4a74755ac2b9
kubikowski <kubikowski0@gmail.com>
2021-03-21 12:09:34 -0400

    The subscription instanceof checks are not a valid solution
    But I have fixed the margins on the snek

--- src/app/pages/page-not-found/snek/snek.component.ts
+++ src/app/pages/page-not-found/snek/snek.component.ts
@@ -1,53 +1,88 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {Component, HostListener, OnDestroy} from '@angular/core';
+import {Observable, of, Subscription} from 'rxjs';
+import {timer} from 'rxjs/internal/observable/timer';
 import {Observed} from 'src/app/shared/decorators/observed.decorator';
+import {SnekDirection} from 'src/app/pages/page-not-found/snek/models/snek-direction.enum';
 import {SnekGame} from 'src/app/pages/page-not-found/snek/models/snek-game.model';
 import {SnekGridNodeType} from 'src/app/pages/page-not-found/snek/models/snek-grid-node-type.enum';
-import {Observable} from 'rxjs';
-import {timer} from 'rxjs/internal/observable/timer';
-import {SubSink} from 'subsink';
-import {SnekDirection} from './models/snek-direction.enum';
+import {catchError, tap} from 'rxjs/operators';
 
 @Component({
 	selector: 'snek',
 	templateUrl: './snek.component.html',
 	styleUrls: ['./snek.component.scss']
 })
-export class SnekComponent implements OnInit {
-	private readonly subscriptions = new SubSink();
+export class SnekComponent implements OnDestroy {
+	private subscription: Subscription;
 
-	@Observed() private snekGame: SnekGame = SnekGame.new(35, 25);
+	@Observed() private snekGame = SnekGame.new(35, 25);
 	public readonly snekGame$: Observable<SnekGame>;
 
 	public readonly SnekGridNodeType = SnekGridNodeType;
 
-	constructor() {
-		this.subscriptions.sink = timer(150, 150)
-			.subscribe(() => this.snekGame.snek.move());
-	}
+	constructor() {}
 
 	@HostListener('document:keydown', [ '$event' ])
 	private keyDown(keyboardEvent: KeyboardEvent): void {
 		switch (keyboardEvent.key) {
 			case 'w':
 			case 'ArrowUp':
+				this.play();
 				this.snekGame.snek.direction = SnekDirection.UP;
 				break;
 			case 's':
 			case 'ArrowDown':
+				this.play();
 				this.snekGame.snek.direction = SnekDirection.DOWN;
 				break;
 			case 'a':
 			case 'ArrowLeft':
+				this.play();
 				this.snekGame.snek.direction = SnekDirection.LEFT;
 				break;
 			case 'd':
 			case 'ArrowRight':
+				this.play();
 				this.snekGame.snek.direction = SnekDirection.RIGHT;
+				break;
+			case 'Escape':
+				this.pause();
+				break;
+			case 'p':
+			case 'Space':
+				(this.subscription instanceof Subscription)
+					? this.pause()
+					: this.play();
 				break;
 		}
 	}
 
-	ngOnInit(): void {
+	private play(): void {
+		if (!(this.subscription instanceof Subscription)) {
+			console.log('subscribed');
+			this.subscription = timer(150, 150)
+				.pipe(tap(() => this.snekGame.snek.move()),
+					catchError(error => {
+						this.reset();
+						return of(error.message);
+					}))
+				.subscribe();
+		}
 	}
 
+	private pause(): void {
+		console.log('unsubscribed');
+		this.subscription.unsubscribe();
+	}
+
+	private reset(): void {
+		this.pause();
+		const length = this.snekGame.snek.length;
+		console.log('new high score: ' + length);
+		this.snekGame = SnekGame.new(35, 25);
+	}
+
+	ngOnDestroy(): void {
+		this.pause();
+	}
 }
