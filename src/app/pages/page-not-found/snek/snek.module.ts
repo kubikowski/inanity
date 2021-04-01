36a8d46fa9e84bf335b11dea2d88a9272850f2af
kubikowski <44616042+kubikowski@users.noreply.github.com>
2021-04-01 14:48:56 -0400

    Added SnekResultDialog (#53)
    * Added a snek results dialog using Angular Material dialogs
    
    * fixed unit test

--- src/app/pages/page-not-found/snek/snek.module.ts
+++ src/app/pages/page-not-found/snek/snek.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SnekComponent } from 'src/app/pages/page-not-found/snek/snek.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { SnekComponent, SnekResultsComponent } from 'src/app/pages/page-not-found/snek/snek.component';
 
 @NgModule({
 	declarations: [
 		SnekComponent,
+		SnekResultsComponent,
 	],
 	imports: [
 		CommonModule,
+		MatButtonModule,
+		MatDialogModule,
 		MatIconModule,
 	],
 	exports: [
 		SnekComponent,
+		SnekResultsComponent,
 	],
 })
 export class SnekModule {
