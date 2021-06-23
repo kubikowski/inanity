9d004d91f1fd685d82f19489ec53a54a56f13ac9
kubikowski <kubikowski0@gmail.com>
2021-06-23 13:00:47 -0700

    Added external icons to MatIconRegistry

--- src/app/shared/svg/svg-icon.enum.ts
+++ src/app/shared/svg/svg-icon.enum.ts
@@ -1,7 +1,11 @@
-export enum SvgIcon {
-	GOAT = 'goat',
-	GONG = 'gong',
-	ONI = 'oni',
-	STACK = 'stack',
-	WHEEL = 'wheel',
-}
+import { ExternalSvgIcon } from 'src/app/shared/svg/external-svg-icon.enum';
+import { InternalSvgIcon } from 'src/app/shared/svg/internal-svg-icon.enum';
+
+type SvgIconKey = keyof typeof InternalSvgIcon | keyof typeof ExternalSvgIcon;
+type SvgIcon = { [ key in SvgIconKey]: key };
+
+const svgIcons = { ...InternalSvgIcon, ...ExternalSvgIcon } as const;
+const svgIconEntries = Object.keys(svgIcons)
+	.map(key => [ key, key ]);
+
+export const SvgIcon = Object.fromEntries(svgIconEntries) as SvgIcon;
