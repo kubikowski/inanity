cda9362e71b5550ee7af9526459d30c827525424
kubikowski <44616042+kubikowski@users.noreply.github.com>
2021-06-24 14:02:24 -0400

    renamed PageNotFound → NotFound (#132)
    * renamed PageNotFound → NotFound
    
    * No more compiler warnings

--- src/app/pages/not-found/snek/snek.module.ts
+++ src/app/pages/not-found/snek/snek.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { SnekComponent, SnekResultsComponent } from 'src/app/pages/page-not-found/snek/snek.component';
+import { SnekComponent, SnekResultsComponent } from 'src/app/pages/not-found/snek/snek.component';
 
 @NgModule({
 	declarations: [
