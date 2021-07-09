7f09eb03d4d39b219f4722511c2b763338552a05
kubikowski <kubikowski0@gmail.com>
2021-07-09 13:48:54 -0600

    formally remove hammerjs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import 'hammerjs';
 import { AppModule } from 'src/app/app.module';
 import { environment } from 'src/environments/environment';
 
