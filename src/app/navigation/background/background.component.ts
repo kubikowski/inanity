329013e8068b4c36a0475b2ac45c0a460ae33721
kubikowski <kubikowski0@gmail.com>
2021-07-19 11:37:52 -0600

    @ViewChild set fails initial getBoundingClientRect

--- src/app/navigation/background/background.component.ts
+++ src/app/navigation/background/background.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { distinctUntilChanged, map } from 'rxjs/operators';
 import { BackgroundCanvasService } from 'src/app/navigation/background/services/background-canvas.service';
@@ -11,8 +11,11 @@ import { FpsService } from 'src/app/shared/screen-detector/fps.service';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	providers: [ BackgroundCanvasService ],
 })
-export class BackgroundComponent {
+export class BackgroundComponent implements AfterViewInit {
 	public readonly fps$: Observable<number>;
+
+	@ViewChild('canvas', { static: true })
+	private readonly canvas: ElementRef<HTMLCanvasElement>;
 
 	constructor(
 		private readonly backgroundCanvasService: BackgroundCanvasService,
@@ -22,8 +25,7 @@ export class BackgroundComponent {
 			.pipe(map(fps => Math.floor(fps)), distinctUntilChanged());
 	}
 
-	@ViewChild('canvas', { static: true })
-	private set canvas(canvasRef: ElementRef<HTMLCanvasElement>) {
-		this.backgroundCanvasService.initialize(canvasRef.nativeElement);
+	ngAfterViewInit(): void {
+		this.backgroundCanvasService.initialize(this.canvas.nativeElement);
 	}
 }
