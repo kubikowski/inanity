4ad53ed81410638fe9394039771fe9d15b1c83ed
kubikowski <kubikowski0@gmail.com>
2021-07-29 19:09:44 -0600

    deleted accidental files

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-	selector: 'app-about',
+	selector: 'about-page',
 	templateUrl: './about.component.html',
-	styleUrls: [ './about.component.scss' ]
+	styleUrls: [ './about.component.scss' ],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
 	constructor() {
-	}
-
-	ngOnInit(): void {
 	}
 
 }
