de8892d092729eaf240d96fde75ab0d28d106686
kubikowski <44616042+kubikowski@users.noreply.github.com>
2021-07-29 19:48:47 -0600

    refactored about to current standards (v0.6.3) (#199)
    * deleted accidental files
    
    * renamed files to `about-page`
    
    * brought the existing about page up to current standards
    
    * and some padding for good taste

--- src/app/pages/about/about-page.module.ts
+++ src/app/pages/about/about-page.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CoreModule } from 'src/app/core/core.module';
 import { DyslexicTextModule } from 'src/app/features/dyslexic-text/dyslexic-text.module';
-import { AboutRoutingModule } from 'src/app/pages/about/about-routing.module';
-import { AboutComponent } from 'src/app/pages/about/about.component';
+import { AboutPageRoutingModule } from 'src/app/pages/about/about-page-routing.module';
+import { AboutPageComponent } from 'src/app/pages/about/about-page.component';
 
 @NgModule({
 	declarations: [
-		AboutComponent,
+		AboutPageComponent,
 	],
 	imports: [
 		CoreModule,
 		DyslexicTextModule,
 	],
 	exports: [
-		AboutRoutingModule,
+		AboutPageRoutingModule,
 	],
 })
-export class AboutModule { }
+export class AboutPageModule { }
