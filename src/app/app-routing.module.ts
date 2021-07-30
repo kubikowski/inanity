9430ceab6ad40ed8cf1c2b63a4f4728858ca9810
kubikowski <kubikowski0@gmail.com>
2021-07-30 12:30:11 -0600

    added route feature flags

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeatureFlagGuard } from 'src/app/core/guards/feature-flag.guard';
 
 const routes: Routes = [
 	{
 		path: '',
 		redirectTo: '/snek',
-		pathMatch: 'full'
+		pathMatch: 'full',
 	}, {
-		path: 'background',
-		loadChildren: () => import('src/app/pages/background/background-page.module')
-			.then(module => module.BackgroundPageModule),
-	}, {
-		path: 'gong',
-		loadChildren: () => import('src/app/pages/gong/gong-page.module')
-			.then(module => module.GongPageModule),
-	}, {
-		path: 'snek',
-		loadChildren: () => import('src/app/pages/snek/snek-page.module')
-			.then(module => module.SnekPageModule),
-	}, {
-		path: '**',
-		loadChildren: () => import('src/app/pages/not-found/not-found-page.module')
-			.then(module => module.NotFoundPageModule),
+		path: '',
+		canActivateChild: [ FeatureFlagGuard ],
+		children: [
+			{
+				path: 'background',
+				loadChildren: () => import('src/app/pages/background/background-page.module')
+					.then(module => module.BackgroundPageModule),
+			}, {
+				path: 'gong',
+				loadChildren: () => import('src/app/pages/gong/gong-page.module')
+					.then(module => module.GongPageModule),
+			}, {
+				path: 'snek',
+				loadChildren: () => import('src/app/pages/snek/snek-page.module')
+					.then(module => module.SnekPageModule),
+			}, {
+				path: '**',
+				loadChildren: () => import('src/app/pages/not-found/not-found-page.module')
+					.then(module => module.NotFoundPageModule),
+			},
+		],
 	},
 ];
 
