b93900b18fbae8e6cb5ae0ebbdf8ea5fd4711d72
kubikowski <kubikowski0@gmail.com>
2021-08-02 23:52:14 -0600

    converted `FaviconService` to use `Renderer2`

--- src/app/core/browser/favicon.service.ts
+++ src/app/core/browser/favicon.service.ts
@@ -1,39 +1,50 @@
 import { DOCUMENT } from '@angular/common';
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 import { FaviconRef } from 'src/app/core/svg/favicon-ref.enum';
 
 @Injectable({ providedIn: 'root' })
 export class FaviconService {
+	private readonly renderer: Renderer2;
 
 	constructor(
 		@Inject(DOCUMENT) private readonly document: HTMLDocument,
+		private readonly rendererFactory: RendererFactory2,
 	) {
+		this.renderer = this.rendererFactory.createRenderer(this.document.head, null);
+
 		this.replaceFavicon(FaviconRef.getDefault());
 	}
 
 	private replaceFavicon(faviconRef: FaviconRef): void {
-		const favicon = this.document.getElementById('favicon');
+		const favicon = this.favicon;
 
 		if (favicon?.getAttribute('href') !== faviconRef) {
-			this.removeFavicon();
+			this.removeFavicon(favicon);
 			this.addFavicon(faviconRef);
 		}
 	}
 
-	private removeFavicon(): void {
-		const favicon = this.document.getElementById('favicon');
-
-		favicon?.parentNode.removeChild(favicon);
+	private removeFavicon(favicon: HTMLLinkElement): void {
+		if (typeof favicon !== 'undefined')
+			this.renderer.removeChild(this.document.head, favicon);
 	}
 
 	private addFavicon(faviconRef: FaviconRef): void {
-		const favicon = this.document.createElement('link');
+		const favicon = this.renderer.createElement('link');
 
-		favicon.setAttribute('id', 'favicon');
-		favicon.setAttribute('rel', 'icon');
-		favicon.setAttribute('type', 'image/svg+xml');
-		favicon.setAttribute('href', faviconRef);
+		this.renderer.setAttribute(favicon, 'rel', 'icon');
+		this.renderer.setAttribute(favicon, 'type', 'image/svg+xml');
+		this.renderer.setAttribute(favicon, 'href', faviconRef);
 
-		this.document.head.appendChild(favicon);
+		this.renderer.appendChild(this.document.head, favicon);
+	}
+
+	private get favicon(): HTMLLinkElement {
+		return Array.from(this.favicons)
+			.find(favicon => favicon.getAttribute('type') === 'image/svg+xml');
+	}
+
+	private get favicons(): NodeListOf<HTMLLinkElement> {
+		return this.document.head.querySelectorAll('link[rel="icon"]');
 	}
 }
