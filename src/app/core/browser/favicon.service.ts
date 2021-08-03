01fb1c711502136e17b140f85edeeb4dd2d88931
kubikowski <kubikowski0@gmail.com>
2021-08-03 00:23:31 -0600

    converted `ColorsService` to use `Renderer2`

--- src/app/core/browser/favicon.service.ts
+++ src/app/core/browser/favicon.service.ts
@@ -4,19 +4,21 @@ import { FaviconRef } from 'src/app/core/svg/favicon-ref.enum';
 
 @Injectable({ providedIn: 'root' })
 export class FaviconService {
+	private readonly head: HTMLHeadElement;
 	private readonly renderer: Renderer2;
 
 	constructor(
 		@Inject(DOCUMENT) private readonly document: HTMLDocument,
 		private readonly rendererFactory: RendererFactory2,
 	) {
-		this.renderer = this.rendererFactory.createRenderer(this.document.head, null);
+		this.head = this.document.head;
+		this.renderer = this.rendererFactory.createRenderer(this.head, null);
 
 		this.replaceFavicon(FaviconRef.getDefault());
 	}
 
 	private replaceFavicon(faviconRef: FaviconRef): void {
-		const favicon = this.favicon;
+		const favicon = this.svgFavicon;
 
 		if (favicon?.getAttribute('href') !== faviconRef) {
 			this.removeFavicon(favicon);
@@ -26,7 +28,7 @@ export class FaviconService {
 
 	private removeFavicon(favicon: HTMLLinkElement): void {
 		if (typeof favicon !== 'undefined')
-			this.renderer.removeChild(this.document.head, favicon);
+			this.renderer.removeChild(this.head, favicon);
 	}
 
 	private addFavicon(faviconRef: FaviconRef): void {
@@ -36,15 +38,20 @@ export class FaviconService {
 		this.renderer.setAttribute(favicon, 'type', 'image/svg+xml');
 		this.renderer.setAttribute(favicon, 'href', faviconRef);
 
-		this.renderer.appendChild(this.document.head, favicon);
+		this.renderer.insertBefore(this.head, favicon, this.appleTouchIcon);
 	}
 
-	private get favicon(): HTMLLinkElement {
+	private get svgFavicon(): HTMLLinkElement {
 		return Array.from(this.favicons)
 			.find(favicon => favicon.getAttribute('type') === 'image/svg+xml');
 	}
 
+	private get appleTouchIcon(): HTMLLinkElement {
+		return Array.from(this.favicons)
+			.find(favicon => favicon.getAttribute('type') === 'apple-touch-icon');
+	}
+
 	private get favicons(): NodeListOf<HTMLLinkElement> {
-		return this.document.head.querySelectorAll('link[rel="icon"]');
+		return this.head.querySelectorAll('link[rel="icon"]');
 	}
 }
--- src/app/core/colors/services/colors.service.ts
+++ src/app/core/colors/services/colors.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable, OnDestroy, Renderer2, RendererFactory2 } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BaseColorPalette } from 'src/app/core/colors/models/color-palettes/base-color-palette.model';
 import { ColorPalette } from 'src/app/core/colors/models/color-palettes/color-palette.model';
@@ -12,6 +13,10 @@ import { SubSink } from 'subsink';
 @Injectable({ providedIn: 'root' })
 export class ColorsService implements OnDestroy {
 	private readonly subscriptions = new SubSink();
+	private readonly renderer: Renderer2;
+
+	private readonly body: HTMLElement;
+	private readonly element: HTMLElement;
 
 	@Observed() public theme: ColorTheme;
 	@Observed() public palette: ColorPalette;
@@ -19,13 +24,19 @@ export class ColorsService implements OnDestroy {
 	public readonly theme$: Observable<ColorTheme>;
 	public readonly palette$: Observable<ColorPalette>;
 
-	constructor() {
+	constructor(
+		@Inject(DOCUMENT) private readonly document: HTMLDocument,
+		private readonly rendererFactory: RendererFactory2,
+	) {
+		this.body = this.document.body;
+		this.element = this.document.documentElement;
+		this.renderer = this.rendererFactory.createRenderer(this.body, null);
+
 		this.theme = ColorsService.localStorageTheme;
 		this.palette = ColorsService.localStoragePalette;
 
 		this.subscriptions.sink = this.theme$
 			.subscribe(theme => this.setTheme(theme));
-
 		this.subscriptions.sink = this.palette$
 			.subscribe(palette => this.setPalette(palette));
 	}
@@ -37,16 +48,16 @@ export class ColorsService implements OnDestroy {
 	// region setters
 	private setTheme(theme: ColorTheme): void {
 		ColorsService.localStorageTheme = theme;
-		ColorsService.documentBodyThemeClass = theme;
-		ColorsService.cssThemeVariables = theme;
+		this.documentBodyThemeClass = theme;
+		this.cssThemeVariables = theme;
 
-		ColorsService.cssPaletteVariables = this.computedPalette;
+		this.cssPaletteVariables = this.computedPalette;
 	}
 
 	private setPalette(palette: ColorPalette): void {
 		ColorsService.localStoragePalette = palette;
 
-		ColorsService.cssPaletteVariables = this.computedPalette;
+		this.cssPaletteVariables = this.computedPalette;
 	}
 
 	private get computedPalette(): ColorPalette {
@@ -68,20 +79,20 @@ export class ColorsService implements OnDestroy {
 		localStorage.setItem('theme', theme.themeName);
 	}
 
-	private static set documentBodyThemeClass(theme: ColorTheme) {
-		const themeNames = ColorThemes.map(colorTheme => colorTheme.themeName);
+	private set documentBodyThemeClass(theme: ColorTheme) {
+		ColorThemes.forEach(colorTheme =>
+			this.renderer.removeClass(this.body, colorTheme.themeName));
 
-		document.body.classList.remove(...themeNames);
-		document.body.classList.add(theme.themeName);
+		this.renderer.addClass(this.body, theme.themeName);
 	}
 
-	private static set cssThemeVariables(theme: ColorTheme) {
+	private set cssThemeVariables(theme: ColorTheme) {
 		const cssThemeEntries = Object.entries(BaseColorTheme.CssThemeVariables);
 
 		cssThemeEntries.forEach(([ themeKey, cssVariableName ]) => {
 			const cssVariableValue = theme[themeKey];
 
-			document.documentElement.style.setProperty(cssVariableName, cssVariableValue);
+			this.renderer.setStyle(this.element, cssVariableName, cssVariableValue, 2);
 		});
 	}
 	// endregion theme handling
@@ -98,13 +109,13 @@ export class ColorsService implements OnDestroy {
 		localStorage.setItem('palette', palette.paletteName);
 	}
 
-	private static set cssPaletteVariables(palette: ColorPalette) {
+	private set cssPaletteVariables(palette: ColorPalette) {
 		const cssPaletteEntries = Object.entries(BaseColorPalette.CssPaletteVariables);
 
 		cssPaletteEntries.forEach(([ paletteKey, cssVariableName ]) => {
 			const cssVariableValue = palette[paletteKey];
 
-			document.documentElement.style.setProperty(cssVariableName, cssVariableValue);
+			this.renderer.setStyle(this.element, cssVariableName, cssVariableValue, 2);
 		});
 	}
 	// endregion palette handling
