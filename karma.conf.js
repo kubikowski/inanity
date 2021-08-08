995e9decdc8982ca410fcbf2406dad852d642bf8
kubikowski <44616042+kubikowski@users.noreply.github.com>
2021-08-07 19:18:49 -0600

    added `.gitattributes` to enforce LF line endings (#225)

--- karma.conf.js
+++ karma.conf.js
@@ -4,28 +4,28 @@
 module.exports = function (config) {
 	config.set({
 		basePath: '',
-		frameworks: ['jasmine', '@angular-devkit/build-angular'],
+		frameworks: [ 'jasmine', '@angular-devkit/build-angular' ],
 		plugins: [
 			require('karma-jasmine'),
 			require('karma-chrome-launcher'),
 			require('karma-jasmine-html-reporter'),
 			require('karma-coverage-istanbul-reporter'),
-			require('@angular-devkit/build-angular/plugins/karma')
+			require('@angular-devkit/build-angular/plugins/karma'),
 		],
 		client: {
-			clearContext: false // leave Jasmine Spec Runner output visible in browser
+			clearContext: false, // leave Jasmine Spec Runner output visible in browser
 		},
 		coverageIstanbulReporter: {
 			dir: require('path').join(__dirname, './coverage/inanity'),
-			reports: ['html', 'lcovonly', 'text-summary'],
-			fixWebpackSourcePaths: true
+			reports: [ 'html', 'lcovonly', 'text-summary' ],
+			fixWebpackSourcePaths: true,
 		},
-		reporters: ['progress', 'kjhtml'],
+		reporters: [ 'progress', 'kjhtml' ],
 		port: 9876,
 		colors: true,
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
-		browsers: ['Chrome'],
+		browsers: [ 'Chrome' ],
 		customLaunchers: {
 			ChromeHeadlessCI: {
 				base: 'ChromeHeadless',
@@ -34,11 +34,11 @@ module.exports = function (config) {
 					'--no-first-run',
 					'--disable-gpu',
 					'--disable-translate',
-					'--disable-extensions'
+					'--disable-extensions',
 				],
 			},
 		},
 		singleRun: false,
-		restartOnFileChange: true
+		restartOnFileChange: true,
 	});
 };
