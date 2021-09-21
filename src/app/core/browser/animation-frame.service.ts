85d15f6ece91eafb2078b37647b7d9c82c350026
kubikowski <44616042+kubikowski@users.noreply.github.com>
2021-09-21 15:23:28 -0600

    `AnimationFrameService` emits `onAnimationFrame$` (#272)
    * `AnimationFrameService` emits `onAnimationFrame$`
    
    * more parenthesis

--- src/app/core/browser/animation-frame.service.ts
+++ src/app/core/browser/animation-frame.service.ts
@@ -11,21 +11,15 @@ export class AnimationFrameService implements OnDestroy {
 	private static enabled = true;
 	private static frameAverage = 60;
 
-	@Observed() private timeStamp: DOMHighResTimeStamp;
-	private readonly timeStamp$: Observable<DOMHighResTimeStamp>;
-
+	@Observed() private onAnimationFrame: DOMHighResTimeStamp;
 	@Observed() private fps = 0;
+
+	public readonly onAnimationFrame$: Observable<DOMHighResTimeStamp>;
 	public readonly fps$: Observable<number>;
 
 	constructor() {
-		this.onAnimationFrame(performance.now());
-
-		this.subscriptions.sink = this.timeStamp$
-			.pipe(
-				scan((acc, timestamp) => [ ...acc.slice(acc.length <= AnimationFrameService.frameAverage ? 0 : 1), timestamp ], []),
-				map(timestamps => timestamps[timestamps.length - 1] - timestamps[0]),
-				map(msBetweenTimestamps => 1000 * AnimationFrameService.frameAverage / msBetweenTimestamps),
-			).subscribe(fps => this.fps = fps);
+		this.requestAnimationFrame(performance.now());
+		this.calculateFps();
 	}
 
 	ngOnDestroy(): void {
@@ -33,11 +27,20 @@ export class AnimationFrameService implements OnDestroy {
 		AnimationFrameService.enabled = false;
 	}
 
-	private onAnimationFrame(time: number): void {
-		this.timeStamp = time;
+	private requestAnimationFrame(time: DOMHighResTimeStamp): void {
+		this.onAnimationFrame = time;
 
 		if (AnimationFrameService.enabled) {
-			requestAnimationFrame(this.onAnimationFrame.bind(this));
+			requestAnimationFrame(this.requestAnimationFrame.bind(this));
 		}
+	}
+
+	private calculateFps(): void {
+		this.subscriptions.sink = this.onAnimationFrame$
+			.pipe(
+				scan((acc, timestamp) => [ ...acc.slice((acc.length <= AnimationFrameService.frameAverage) ? 0 : 1), timestamp ], []),
+				map(timestamps => timestamps[timestamps.length - 1] - timestamps[0]),
+				map(msBetweenTimestamps => 1000 * AnimationFrameService.frameAverage / msBetweenTimestamps),
+			).subscribe(fps => this.fps = fps);
 	}
 }
