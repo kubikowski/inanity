0e1fc9089c17b164eddee8e4c508e4acf3577d6e
kubikowski <44616042+kubikowski@users.noreply.github.com>
2021-10-03 23:54:37 -0600

    added a clock (#282)
    * added a primitave clock. nothing fun about it yet
    
    * added `ClockUtil` methods
    
    * moved the fps and clock into a single region

---
+++ src/app/features/clock/clock.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { Observed } from 'rxjs-observed-decorator';
+import { map } from 'rxjs/operators';
+import { ClockUtil } from 'src/app/features/clock/clock-util.function';
+import { SubSink } from 'subsink';
+
+@Component({
+	selector: 'clock',
+	template: `{{ time$ | async | date: 'shortTime' }}`,
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ClockComponent implements OnDestroy {
+	private readonly subscriptions = new SubSink();
+
+	@Observed() private time = ClockUtil.getStartTime();
+	public readonly time$: Observable<Date>;
+
+	public constructor() {
+		this.subscriptions.sink = interval(1000)
+			.pipe(map(() => ClockUtil.countDown(this.time)))
+			.subscribe(time => this.time = time);
+	}
+
+	public ngOnDestroy(): void {
+		this.subscriptions.unsubscribe();
+	}
+}
