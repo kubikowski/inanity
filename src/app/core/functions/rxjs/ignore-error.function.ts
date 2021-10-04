c3e080227f51ad42aee07b5548431224d78d610f
kubikowski <kubikowski0@gmail.com>
2021-10-03 22:54:27 -0600

    condensed functionality of `ignoreError`

--- src/app/core/functions/rxjs/ignore-error.function.ts
+++ src/app/core/functions/rxjs/ignore-error.function.ts
@@ -2,13 +2,37 @@ import { MonoTypeOperatorFunction, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 /**
- * Replaces errored streams with `null` values
+ * Determines what to replace errored streams with.
+ *
+ * * NONE • discards the stream.
+ * * NULL • outputs a `null` stream in place of errors.
+ * * VOID • outputs a `void 0` stream in place of errors.
+ */
+export enum ErrorEmission {
+	NONE = 'NONE',
+	NULL = 'NULL',
+	VOID = 'VOID',
+}
+
+/**
+ * Discards errored streams, or replaces them with `null` or `void` outputs.
  *
  * @example
  * obs$.pipe(ignoreError());
+ * obs$.pipe(ignoreError(ErrorEmission.NULL));
  *
- * @return `of(null)`
+ * @param errorEmission Determines what to replace errored streams with.
+ *
+ * @return `of()`, `of(null)`, or `of(void 0)`, depending on errorEmission.
  */
-export function ignoreError<T>(): MonoTypeOperatorFunction<T> {
-	return catchError(ignored => of<T>(null));
+export function ignoreError<T>(errorEmission = ErrorEmission.NONE): MonoTypeOperatorFunction<T> {
+	switch (errorEmission) {
+		default:
+		case ErrorEmission.NONE:
+			return catchError(ignored => of<T>());
+		case ErrorEmission.NULL:
+			return catchError(ignored => of<T>(null));
+		case ErrorEmission.VOID:
+			return catchError(ignored => of<T>(void 0));
+	}
 }
