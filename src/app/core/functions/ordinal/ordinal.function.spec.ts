fec3d0080d2de9f3882be6950b09955407333869
kubikowski <kubikowski0@gmail.com>
2022-01-13 09:24:42 -0700

    reformatted for style points

--- src/app/core/functions/ordinal/ordinal.function.spec.ts
+++ src/app/core/functions/ordinal/ordinal.function.spec.ts
@@ -2,21 +2,21 @@ import { ordinal } from 'src/app/core/functions/ordinal/ordinal.function';
 
 describe('ordinal', () => {
 	// standards
-	const zeroes = [ -1000, -20, -10, -0, 0, 10, 20, 1000 ] as const;
-	const ones = [ -1001, -21, -1, 1, 21, 1001 ] as const;
-	const twos = [ -2222, -22, -2, 2, 22, 2222 ] as const;
-	const threes = [ -3333, -23, -3, 3, 23, 3333 ] as const;
-	const fours = [ -4444, -24, -14, -4, 4, 14, 24, 4444 ] as const;
-	const fives = [ -5555, -25, -15, -5, 5, 15, 25, 5555 ] as const;
-	const sixes = [ -6666, -26, -16, -6, 6, 16, 26, 6666 ] as const;
-	const sevens = [ -7777, -27, -17, -7, 7, 17, 27, 7777 ] as const;
-	const eights = [ -8888, -28, -18, -8, 8, 18, 28, 8888 ] as const;
-	const nines = [ -9999, -29, -19, -9, 9, 19, 29, 9999 ] as const;
+	const zeroes =  [ -1000, -20, -10, -0, 0, 10, 20, 1000 ] as const;
+	const ones =    [ -1001, -21,      -1, 1,     21, 1001 ] as const;
+	const twos =    [ -2222, -22,      -2, 2,     22, 2222 ] as const;
+	const threes =  [ -3333, -23,      -3, 3,     23, 3333 ] as const;
+	const fours =   [ -4444, -24, -14, -4, 4, 14, 24, 4444 ] as const;
+	const fives =   [ -5555, -25, -15, -5, 5, 15, 25, 5555 ] as const;
+	const sixes =   [ -6666, -26, -16, -6, 6, 16, 26, 6666 ] as const;
+	const sevens =  [ -7777, -27, -17, -7, 7, 17, 27, 7777 ] as const;
+	const eights =  [ -8888, -28, -18, -8, 8, 18, 28, 8888 ] as const;
+	const nines =   [ -9999, -29, -19, -9, 9, 19, 29, 9999 ] as const;
 
 	// outliers: should be 'th'
-	const elevens = [ -1111, -11, 11, 1111 ] as const;
-	const twelves = [ -1212, -12, 12, 1212 ] as const;
-	const thirteens = [ -1313, -13, 13, 1313 ] as const;
+	const elevens =     [ -1111, -11, 11, 1111 ] as const;
+	const twelves =     [ -1212, -12, 12, 1212 ] as const;
+	const thirteens =   [ -1313, -13, 13, 1313 ] as const;
 
 	describe('standards', () => {
 		it('should convert …0 → \'th\'', () => {
