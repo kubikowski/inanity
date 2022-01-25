7938a21644c57cf2c6db745407d0f29e3f3ce529
kubikowski <kubikowski0@gmail.com>
2022-01-25 12:52:51 -0700

    added unit tests for `escapeRegExp` and `sets`

--- src/app/core/functions/reg-exp/escape-reg-exp.function.ts
+++ src/app/core/functions/reg-exp/escape-reg-exp.function.ts
@@ -6,5 +6,5 @@
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
  */
 export function escapeRegExp(value: string): string {
-	return value.replace(/[.*+?^${}()|\[\]\\]/g, '\\$&');
+	return value.replace(/([.*+?^=!:${}()|[\]\/\\])/g, '\\$1');
 }
