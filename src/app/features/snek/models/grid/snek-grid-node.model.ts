24ea3e9bb974d766c1c7b09ff51d96f3dc803fc4
kubikowski <kubikowski0@gmail.com>
2022-03-21 22:18:12 -0600

    snek - removed snek body scss

--- src/app/features/snek/models/grid/snek-grid-node.model.ts
+++ src/app/features/snek/models/grid/snek-grid-node.model.ts
@@ -6,7 +6,7 @@ import { SnekNode } from 'src/app/features/snek/models/snek/snek-node.model';
 
 export class SnekGridNode {
 	@Observed() public type = SnekGridNodeType.BLANK;
-	@Observed() public snekNode: SnekNode | null = null;
+	@Observed() private snekNode: SnekNode | null = null;
 	private _up!: SnekGridNode | null;
 	private _down!: SnekGridNode | null;
 	private _left!: SnekGridNode | null;
