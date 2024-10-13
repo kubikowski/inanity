3e5d926dfe1044243130fd332064dd169a831ccf
Nathaniel Holden <kubikowski0@gmail.com>
2024-10-12 23:52:03 -0700

    font licensing

--- src/app/features/navigation/components/header/header.component.ts
+++ src/app/features/navigation/components/header/header.component.ts
@@ -1,17 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
+import { DyslexicTextComponent } from 'src/app/features/dyslexia/components/dyslexic-text/dyslexic-text.component';
+import { SettingsComponent } from 'src/app/features/navigation/components/settings/settings.component';
 import { NavigationService } from 'src/app/features/navigation/services/navigation.service';
 
 @Component({
 	selector: 'app-header',
-	templateUrl: './header.component.html',
-	styleUrls: [ './header.component.scss' ],
+	templateUrl: 'header.component.html',
+	styleUrl: 'header.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
+	standalone: true,
+	imports: [
+		MatToolbar,
+		MatIconButton,
+		MatIcon,
+		DyslexicTextComponent,
+		SettingsComponent
+	]
 })
 export class HeaderComponent {
-
-	public constructor(
-		private readonly navigationService: NavigationService,
-	) { }
+	private readonly navigationService = inject(NavigationService);
 
 	public toggleSidenav(event: Event): void {
 		(event.currentTarget as HTMLElement).blur();
