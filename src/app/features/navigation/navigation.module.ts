559243ae0f878548ee541fd22bae5b985c2e9aba
Nathaniel Holden <kubikowski0@gmail.com>
2024-10-12 17:59:55 -0700

    import order

--- src/app/features/navigation/navigation.module.ts
+++ src/app/features/navigation/navigation.module.ts
@@ -3,15 +3,15 @@ import { CoreModule } from 'src/app/core/core.module';
 import { DyslexicTextComponent } from 'src/app/features/dyslexia/components/dyslexic-text/dyslexic-text.component';
 import { HeaderComponent } from 'src/app/features/navigation/components/header/header.component';
 import { SettingsComponent } from 'src/app/features/navigation/components/settings/settings.component';
-import { SidebarItemComponent } from 'src/app/features/navigation/components/sidebar-item/sidebar-item.component';
 import { SidebarComponent } from 'src/app/features/navigation/components/sidebar/sidebar.component';
+import { SidebarItemComponent } from 'src/app/features/navigation/components/sidebar-item/sidebar-item.component';
 
 @NgModule({
 	declarations: [
 		HeaderComponent,
 		SettingsComponent,
-		SidebarItemComponent,
 		SidebarComponent,
+		SidebarItemComponent,
 	],
 	imports: [
 		CoreModule,
