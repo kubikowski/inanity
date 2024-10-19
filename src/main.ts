10782e96a5c9efe71009c6a37759fffe42ada18a
Nathaniel Holden <kubikowski0@gmail.com>
2024-10-18 22:54:12 -0700

    rudimentary firebase authentication

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { provideHttpClient } from '@angular/common/http';
 import { enableProdMode } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { MAT_ICON_DEFAULT_OPTIONS } from '@angular/material/icon';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -18,6 +19,7 @@ bootstrapApplication(AppComponent, {
 		provideAnimations(),
 		provideHttpClient(),
 		provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+		provideAuth(() => getAuth()),
 		{ provide: MAT_ICON_DEFAULT_OPTIONS, useValue: { fontSet: 'material-symbols-rounded' } },
 		provideRouter(routes),
 	],
