7c41bb6e8f9eb00e5cc407c96b60edeff783c9ed
Nathaniel Holden <44616042+kubikowski@users.noreply.github.com>
2024-10-19 17:23:09 -0700

    psuedorandom snek (#697)
    * psuedorandom snek
    
    * robust food generation

--- src/app/features/snek/models/state/snek-game.model.ts
+++ src/app/features/snek/models/state/snek-game.model.ts
@@ -1,4 +1,5 @@
 import { signal, untracked } from '@angular/core';
+import { Random } from 'src/app/core/functions/number/random.function';
 import { SnekDirection } from 'src/app/features/snek/models/direction/snek-direction.enum';
 import { SnekGridNodeType } from 'src/app/features/snek/models/grid/snek-grid-node-type.enum';
 import { SnekGridNode } from 'src/app/features/snek/models/grid/snek-grid-node.model';
@@ -7,6 +8,7 @@ import { Snek } from 'src/app/features/snek/models/snek/snek.model';
 export class SnekGame {
 	public static readonly initialSnekLength = 3;
 
+	private readonly _seed: number;
 	private readonly _grid: ReadonlyArray<ReadonlyArray<SnekGridNode>>;
 	private readonly _snek: Snek;
 	private _fudNode!: SnekGridNode;
@@ -19,6 +21,9 @@ export class SnekGame {
 		private readonly _width: number,
 		private readonly _height: number,
 	) {
+		this._seed = Math.floor(Math.random() * 0x10000);
+		Random.seed(this._seed);
+
 		this._grid = Array.from(Array(this._height),
 			(_row, height) => Array.from(Array(this._width),
 				(_node, width) => SnekGridNode.new(width, height)));
@@ -63,8 +68,8 @@ export class SnekGame {
 	}
 
 	private findBlankGridNode(): SnekGridNode {
-		const randomIndex = Math.floor(Math.random() * ((this._width * this._height) - this._snek.length));
-		let currentSnekGridNode = this.at(0, 0) as SnekGridNode;
+		const randomIndex = Random.uniform(0, (this._width * this._height) - this._snek.length);
+		let currentSnekGridNode = this.validateBlankGridNode(this.at(0, 0) as SnekGridNode);
 		for (let currentIndex = 0; currentIndex < randomIndex; currentIndex++) {
 			currentSnekGridNode = this.findNextBlankGridNode(currentSnekGridNode);
 		}
@@ -72,13 +77,26 @@ export class SnekGame {
 	}
 
 	private findNextBlankGridNode(snekGridNode: SnekGridNode): SnekGridNode {
-		const nextGridNode = (snekGridNode.width + 1 === this._width)
-			? this.at(0, snekGridNode.height + 1) as SnekGridNode
-			: snekGridNode.next(SnekDirection.RIGHT) as SnekGridNode;
+		const nextGridNode = this.findNextGridNode(snekGridNode);
+		return this.validateBlankGridNode(nextGridNode);
+	}
 
-		return (untracked(nextGridNode.type) === SnekGridNodeType.BLANK)
-			? nextGridNode
-			: this.findNextBlankGridNode(nextGridNode);
+	private findNextGridNode(snekGridNode: SnekGridNode): SnekGridNode {
+		if (snekGridNode.width + 1 < this._width) {
+			return snekGridNode.next(SnekDirection.RIGHT) as SnekGridNode;
+		} else if (snekGridNode.height + 1 < this._height) {
+			return this.at(0, snekGridNode.height + 1) as SnekGridNode;
+		} else {
+			return this.at(0, 0) as SnekGridNode;
+		}
+	}
+
+	private validateBlankGridNode(snekGridNode: SnekGridNode): SnekGridNode {
+		if (untracked(snekGridNode.type) === SnekGridNodeType.BLANK) {
+			return snekGridNode;
+		} else {
+			return this.findNextBlankGridNode(snekGridNode);
+		}
 	}
 
 	private at(width: number, height: number): SnekGridNode | null {
