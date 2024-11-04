e3e6b2c7cd9fabfbba35c376a5ba46cb152b472a
Nathaniel Holden <kubikowski0@gmail.com>
2024-11-03 20:06:51 -0800

    feature: platform components
    Upgraded `<truncated>` to a component, and added a `<background-image>` component for static background images, complete with `clip-path` options.

---
+++ src/app/core/browser/components/background-image/background-image.component.ts
@@ -0,0 +1,31 @@
+import { ChangeDetectionStrategy, Component, input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+	selector: 'background-image',
+	template: '',
+	styleUrl: 'background-image.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	encapsulation: ViewEncapsulation.None,
+	standalone: true,
+	host: {
+		'[class]': 'clip()',
+		'[class.background-image]': 'true',
+		'[style.background-image]': '\'url(\' + url() + \')\'',
+		'[style.min-height]': 'height() + \'px\'',
+		'[title]': 'alt() ?? \'\'',
+	},
+})
+export class BackgroundImageComponent {
+
+	/** Image Source URL */
+	public readonly url = input.required<string>();
+
+	/** Image Alternate Text */
+	public readonly alt = input<string>();
+
+	/** Scroll Height */
+	public readonly height = input.required<number>();
+
+	/** Whether to clip the image */
+	public readonly clip = input<'parallelogram' | 'wave'>();
+}
