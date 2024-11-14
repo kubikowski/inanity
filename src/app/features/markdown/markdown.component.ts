4f08ec3a9c0f174db46b76dd2512c1a80b230e99
Nathaniel Holden <kubikowski0@gmail.com>
2024-11-13 19:39:56 -0800

    markdown - smartypants

--- src/app/features/markdown/markdown.component.ts
+++ src/app/features/markdown/markdown.component.ts
@@ -4,6 +4,7 @@ import hljs from 'highlight.js';
 import { Marked } from 'marked';
 import { baseUrl } from 'marked-base-url';
 import { markedHighlight } from 'marked-highlight';
+import { markedSmartypants } from 'marked-smartypants';
 import { SubSink } from 'subsink';
 
 @Component({
@@ -19,7 +20,7 @@ export class MarkdownComponent implements OnInit, OnDestroy {
 	private readonly subscriptions = new SubSink();
 
 	public readonly url = input.required<string>();
-	public readonly marked = computed(() => this.initializeMarkdown(this.url()));
+	public readonly marked = computed(() => this.initializeMarked(this.url()));
 	public readonly markdown = signal<string | null>(null);
 
 	public readonly markdownHtml = computed(() => {
@@ -36,17 +37,18 @@ export class MarkdownComponent implements OnInit, OnDestroy {
 		this.subscriptions.unsubscribe();
 	}
 
-	private initializeMarkdown(url: string): Marked {
+	private initializeMarked(url: string): Marked {
 		return new Marked(
 			baseUrl(MarkdownComponent.relativeUrl(url)),
 			markedHighlight({
 				emptyLangClass: 'hljs',
 				langPrefix: 'hljs language-',
-				highlight(code: string, lang: string, _info: string) {
-					const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-					return hljs.highlight(code, { language }).value;
+				highlight(_code: string, _lang: string, _info: string): string {
+					const language = hljs.getLanguage(_lang) ? _lang : 'plaintext';
+					return hljs.highlight(_code, { language }).value;
 				},
 			}),
+			markedSmartypants(),
 		);
 	}
 
