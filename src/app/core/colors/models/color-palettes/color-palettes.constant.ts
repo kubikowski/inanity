9deff28815e5a8bd0b1a555c07c04433eec039ab
Nathaniel Holden <44616042+kubikowski@users.noreply.github.com>
2024-11-18 10:41:12 -0800

    Color Themes v3 (#722)
    * pink palette potential port
    
    * potential emotion colors
    
    * added color name documentation
    
    * purple and red palettes
    
    * updated default colors

--- src/app/core/colors/models/color-palettes/color-palettes.constant.ts
+++ src/app/core/colors/models/color-palettes/color-palettes.constant.ts
@@ -15,14 +15,26 @@ export const BluePalette = new ColorPalette(
 
 export const GreenPalette = new ColorPalette(
 	Color.fromString('#70ee7a'),
-	Color.fromString('#3cd070'),
-	Color.fromString('#50c878'),
-	Color.fromString('#3cb371'),
-	Color.fromString('#00a86b'),
+	Color.fromString('#3cd070'), // UFO Green
+	Color.fromString('#50c878'), // Paris Green
+	Color.fromString('#3cb371'), // Medium Sea Green
+	Color.fromString('#00a86b'), // Jade
 	Color.fromString('#009060'),
-	Color.fromString('#00755e'),
+	Color.fromString('#00755e'), // Pantone 568 CP [Tropical Rain Forest]
 	'green-palette',
 	'Moss',
+);
+
+export const PurplePalette = new ColorPalette(
+	Color.fromString('#dbd5eb'),
+	Color.fromString('#cdc4e3'),
+	Color.fromString('#b2a4d4'), // Pantone 15-3817 TPX Lavender
+	Color.fromString('#9e8dc9'),
+	Color.fromString('#826db0'),
+	Color.fromString('#7058a4'),
+	Color.fromString('#5f4b8b'), // Pantone 18-3838 TCX Ultra Violet
+	'purple-palette',
+	'Lavender',
 );
 
 export const PinkPalette = new ColorPalette(
@@ -37,26 +49,40 @@ export const PinkPalette = new ColorPalette(
 	'Flamingo',
 );
 
+/** rejected in favor of flamingo */
+export const RhubarbPalette = new ColorPalette(
+	Color.fromString('#fdb4c8'),
+	Color.fromString('#fda1ba'),
+	Color.fromString('#fc8eac'),
+	Color.fromString('#e96a97'), // Pantone 16-2126 TCX [Azalea Pink]
+	Color.fromString('#b61c50'), // Pantone 18-1950 TCX [Jazzy]
+	Color.fromString('#a52350'), // Pantone 19-2039 TCX [Granita]
+	Color.fromString('#77202f'), // Pantone 19-1652 TCX [Rhubarb]
+	'pink-palette',
+	'Flamingo',
+);
+
 export const RedPalette = new ColorPalette(
+	Color.fromString('#f77786'),
+	Color.fromString('#f66878'),
+	Color.fromString('#f5495d'),
 	Color.fromString('#f4364c'), // Pantone 1787 C
 	Color.fromString('#ef3340'), // Pantone Red 032 C
 	Color.fromString('#d50032'), // Pantone 199 C
 	Color.fromString('#cd001a'), // Pantone 3546 C
-	Color.fromString('#b80f0a'),
-	Color.fromString('#9b111e'),
-	Color.fromString('#7e0b02'),
 	'red-palette',
 	'Crimson',
 );
 
+/** rejected */
 export const OrangePalette = new ColorPalette(
 	Color.fromString('#ffe808'),
-	Color.fromString('#ffcc00'),
-	Color.fromString('#ffa812'),
-	Color.fromString('#ff7f00'),
-	Color.fromString('#f94d00'),
-	Color.fromString('#e62020'),
-	Color.fromString('#c90016'),
+	Color.fromString('#ffcc00'), // Tangerine Yellow
+	Color.fromString('#ffa812'), // Dark Tangerine
+	Color.fromString('#ff7f00'), // Orange (color wheel)
+	Color.fromString('#f94d00'), // Tangelo
+	Color.fromString('#e62020'), // Lust
+	Color.fromString('#c90016'), // Harvard Crimson
 	'orange-palette',
 	'Combustion',
 );
@@ -76,6 +102,7 @@ export const TanPalette = new ColorPalette(
 export const ColorPalettes = [
 	BluePalette,
 	GreenPalette,
+	PurplePalette,
 	PinkPalette,
 	RedPalette,
 	TanPalette,
