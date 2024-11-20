82ebf78e3261dda0e114c97788fdcaae4a1b87bb
Nathaniel Holden <kubikowski0@gmail.com>
2024-11-20 13:38:20 -0800

    fragment anchor buttons
    And support symlinks in local testing

--- src/app/features/markdown/fragment-anchor.component.ts
+++ src/app/features/markdown/fragment-anchor.component.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MatIconAnchor } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
 	selector: 'fragment-anchor',
-	template: '<a class="fragment-anchor" [href]="link"><mat-icon>double_arrow</mat-icon></a>',
+	template: '<a mat-icon-button class="fragment-anchor" [href]="link"><mat-icon>double_arrow</mat-icon></a>',
 	styleUrl: 'fragment-anchor.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true,
-	imports: [ MatIcon ],
+	imports: [ MatIcon, MatIconAnchor ],
 })
 export class FragmentAnchorComponent {
 	/** at time of writing, createCustomElement does not handle signal inputs */
