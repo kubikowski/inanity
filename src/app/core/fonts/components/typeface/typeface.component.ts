31961d1ef0218143f0a868bd0b6082047be5cde7
Nathaniel Holden <kubikowski0@gmail.com>
2025-09-11 13:10:58 -0700

    half-baked implementation of typeface selection

--- src/app/core/fonts/components/typeface/typeface.component.ts
+++ src/app/core/fonts/components/typeface/typeface.component.ts
@@ -1,5 +1,7 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, effect, inject, input, signal, untracked } from '@angular/core';
 import { FontService } from 'src/app/core/fonts/services/font.service';
+import { timeout } from 'src/app/core/functions/promise/timeout.function';
+import { allowWrites } from 'src/app/core/functions/signal/allow-writes.constant';
 import { NotificationService } from 'src/app/core/notifications/notification.service';
 import { getTypefaceClassificationCss } from '../../models/typeface-classification.enum';
 import { Typeface } from '../../models/typeface.model';
@@ -20,12 +22,44 @@ export class TypefaceComponent {
 	public readonly typeface = input.required<Typeface>();
 	public readonly classification = computed(() => getTypefaceClassificationCss(this.typeface().classification));
 
-	public async applyTypeface(): Promise<void> {
-		this.fontService.typeface.set(this.typeface());
+	private readonly isApplied = computed(() => this.fontService.typeface() === this.typeface());
+	private readonly shouldApply = signal(false);
 
-		if (this.typeface() !== FontService.DEFAULT_TYPEFACE) {
+	// TODO: this solution is getting closer and closer to working...
+	//  currently, this work for spamming the same typeface.
+	//  but alternating between two doesn't work.
+	private readonly shouldRemove = signal(true);
+
+	private readonly isDefaultApplied = computed(() => this.fontService.typeface() === FontService.DEFAULT_TYPEFACE);
+	private readonly canUnapply = computed(() => this.isApplied());
+
+	public constructor() {
+		effect(() => {
+			if (this.shouldApply() && !this.isApplied()) {
+				this.fontService.typeface.set(this.typeface());
+			}
+
+			if (!this.shouldApply() && this.isApplied()) {
+				if (untracked(this.shouldRemove)) {
+					if (this.typeface() !== FontService.DEFAULT_TYPEFACE) {
+						this.fontService.typeface.set(FontService.DEFAULT_TYPEFACE);
+					}
+				} else {
+					this.shouldRemove.set(true);
+				}
+			}
+		}, allowWrites);
+	}
+
+	public async applyTypeface(): Promise<void> {
+		if (!this.isApplied()) {
+			this.shouldApply.set(true);
 			await this.notificationService.notify('we don\'t do that here...');
-			this.fontService.typeface.set(FontService.DEFAULT_TYPEFACE);
+		} else {
+			this.shouldRemove.set(false);
+			await timeout();
 		}
+
+		this.shouldApply.set(false);
 	}
 }
